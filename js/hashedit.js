dc01f85ddd738a94f94183b3acaa3ae5752923ed
Matthew Smith <matt@matthewsmith.com>
2015-12-08 23:46:31 -0500

    animate edit icons

--- js/hashedit.js
+++ js/hashedit.js
@@ -9,7 +9,7 @@ var hashedit = (function () {
     return {
 
         // set version
-        version: '0.3.9',
+        version: '0.4.0',
 
         // set debug messages
         debug: true,
@@ -432,6 +432,13 @@ var hashedit = (function () {
                 span = document.createElement('span');
                 span.setAttribute('class', 'hashedit-remove');
                 span.innerHTML = '<span><svg viewBox="0 0 24 24" height="100%" width="100%" preserveAspectRatio="xMidYMid meet"><g><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></g></svg></span>';
+
+                // append the handle to the wrapper
+                div.appendChild(span);
+
+                span = document.createElement('span');
+                span.setAttribute('class', 'hashedit-edit');
+                span.innerHTML = '<span><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet"><g><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></g></svg></span>';
 
                 // append the handle to the wrapper
                 div.appendChild(span);
